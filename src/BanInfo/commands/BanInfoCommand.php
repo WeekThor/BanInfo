d483f1bc196978b95f10c8ac76c1b57ca83c6f6e
TSt <vitktos@gmail.com>
2020-11-15 07:10:09 +0500

    Add files via upload

---
+++ src/BanInfo/commands/BanInfoCommand.php
@@ -0,0 +1,46 @@
+<?php
+namespace BanInfo\commands;
+
+use BanInfo\Loader;
+use BanInfo\APIs\BanInfo;
+use BanInfo\APIs\API;
+use BanInfo\APIs\DateFormatter;
+use pocketmine\command\CommandSender;
+
+class BanInfoCommand extends API{
+	public function __construct(Loader $plugin){
+        parent::__construct($plugin, "baninfo", "Information about ban", "/bi <nick>", null, ["bi", "tbi"]);
+        $this->setPermission("baninfo.commands.baninfo");
+    }
+
+	public function execute(CommandSender $sender, $currentAlias, array $args){
+	    $dateFormatter = new DateFormatter();
+	    if(!$this->testPermission($sender)){
+	        return true;
+	    }
+	    
+	    if(count($args) === 0){
+	        $sender->sendMessage("§4Use: §c/bi <nick>");
+	        
+	        return false;
+	    }
+	    $value = array_shift($args);
+	    $banInfoClass = new BanInfo($sender->getServer()->getDataPath() . 'banned-players.txt');
+	    $baninfo = $banInfoClass->get($value);
+	    
+	    if($baninfo == null){
+	        $sender->sendMessage('§4[BanInfo] §cError: player not banned.');
+	    }else{
+	        $date = date('j M Y H:i:s', $baninfo->bannedDate);
+	        if($baninfo->unbanDate != null){
+	            $until = date('j M Y H:i:s', $baninfo->unbanDate);
+	        }else{
+	            $until = "Never";
+	        }
+	        if($baninfo->reason == ''){
+	            $baninfo->reason = "§7(not specified)";
+	        }
+	        $sender->sendMessage("§6--=== §c".$baninfo->player."§6 ===--\n§6Banned:§c ".$date."\n§6Banned by: §c".$baninfo->bannedBy."\n§6Ban until: §c".$until."\n§6Ban reason: §c".$baninfo->reason);
+	    }
+    }
+}
